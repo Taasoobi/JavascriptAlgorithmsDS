bfe3e8f25e5b67193bc2cab75a37613049b3a2e4
ZeNoobi <ctaaseen@gmail.com>
2024-01-12 16:59:57 -0800

    refined Bubble Sort

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -16,4 +16,20 @@ function myBubbleSort(arr){
     return arr;
 }
 
-console.log(myBubbleSort([1,18,4,7,2,25,33,3,12]));
+//console.log(myBubbleSort([1,18,4,7,2,25,33,3,12]));
+
+function refinedBSort(arr){
+    for(let i = arr.length; i>0; i--){
+        for(let j = 0; j<i-1; j++){
+            console.log(arr, arr[j], arr[j+1]);
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(refinedBSort([37,45,29,8]));
