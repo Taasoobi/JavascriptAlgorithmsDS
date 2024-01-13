529b766087907a545bf068f7b4ca7ed7193428fd
ZeNoobi <ctaaseen@gmail.com>
2024-01-12 20:00:45 -0800

    High to Low sorted?

--- Sorting Algorithms/selectionSort.js
+++ Sorting Algorithms/selectionSort.js
@@ -1,1 +1,24 @@
 
+function mySelectionSort(arr){
+let min = arr[0];
+
+for(let i = 0; i< arr.length; i++){
+    console.log(arr);
+    min = arr[i];
+    for(let j = 0; j< arr.length; j++){
+        if(arr[j] < min){
+            
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            
+        }
+    }
+    //arr[i] = min;
+}
+
+return arr;
+
+}
+
+console.log(mySelectionSort([5,3,4,1,2]));
