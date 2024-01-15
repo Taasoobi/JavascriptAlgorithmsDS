3df5892ae3909009600d834c2a1053e2ea4ced37
ZeNoobi <ctaaseen@gmail.com>
2024-01-15 09:28:39 -0800

    Final Attempt at Personal Selection Sort

--- Sorting Algorithms/selectionSort.js
+++ Sorting Algorithms/selectionSort.js
@@ -1,24 +1,20 @@
 
 function mySelectionSort(arr){
-let min = arr[0];
+let min;
 
 for(let i = 0; i< arr.length; i++){
     console.log(arr);
     min = arr[i];
-    for(let j = 0; j< arr.length; j++){
-        if(arr[j] < min){
-            
+    for(let j = i; j< arr.length; j++){
+        if(arr[j] < min){            
             let temp = arr[i];
             arr[i] = arr[j];
-            arr[j] = temp;
-            
+            arr[j] = temp;            
         }
     }
     //arr[i] = min;
 }
-
 return arr;
-
 }
+console.log(mySelectionSort([115,23,41,101,82,53])); // could not personally implement selection sort.
 
-console.log(mySelectionSort([5,3,4,1,2]));
