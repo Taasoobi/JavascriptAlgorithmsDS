d88e6a751afee368efb869a5acf8150289341721
ZeNoobi <ctaaseen@gmail.com>
2024-01-17 18:33:40 -0800

    a

--- Sorting Algorithms/selectionSort.js
+++ Sorting Algorithms/selectionSort.js
@@ -1,3 +1,8 @@
+/**
+BigO of Selection Sort
+O(n^2)
+ */
+
 
 function mySelectionSort(arr){
 let min;
@@ -17,9 +22,27 @@ for(let i = 0; i< arr.length; i++){
 }
 return arr;
 }
-console.log(mySelectionSort([1115,123,41,101,8,453])); // could not personally implement selection sort.
+//console.log(mySelectionSort([1115,123,41,101,8,453])); // could not personally implement selection sort.
 
 function refinedSelectionSort(arr){
-
+    for(let i = 0; i<arr.length;i++){
+        let lowest = i;
+        for(let j = i+1; j<arr.length; j++){
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+        //console.log(arr);
+        //console.log("Swapping");
+        let temp = arr[i];
+        arr[i] = arr[lowest];
+        arr[lowest] = temp;
+        //console.log(arr);
+        //console.log("_____________________");
+        }
+    }
+    return arr;
 }
 
+console.log(refinedSelectionSort([34,22,10,19,17]));
