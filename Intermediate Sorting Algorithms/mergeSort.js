fbd6b0c9ea246ae9a9c55596061f768027729a03
ZeNoobi <ctaaseen@gmail.com>
2024-01-18 17:05:50 -0800

    Refined Merge Sort Completed

--- Intermediate Sorting Algorithms/mergeSort.js
+++ Intermediate Sorting Algorithms/mergeSort.js
@@ -1,6 +1,12 @@
 
 function myMergeSort(arr){
+    if(arr.length <= 1){return arr;}
+    let mid = Math.floor(arr.length/2);
+    let arr1 = arr.slice(0, mid);
+    let arr2 = arr.slice(mid);
+    
 
+    return refinedMergeSort(refinedMerge());
 }
 
 function myMerge(ar1, ar2){
@@ -17,28 +23,59 @@ function myMerge(ar1, ar2){
             i++;
         }
     }
-    if(i == ar1.length){
+    if(i == ar1.length || j == ar2.length){
         while(j != ar2.length){
             arr.push(ar2[j]);
             j++;
         }
-    }
-    if(j == ar2.length){
         while(i != ar1.length){
             arr.push(ar1[i]);
             i++;
         }
-    }    
+    }
+  
     return arr;    
 }
 
-console.log(myMerge([1,10,50], [2,14,99,100]));
+//console.log(myMerge([1,5,77], [2,14,99, 100]));
 
 //----------------------
 
 function refinedMergeSort(arr){
+    if(arr.length <= 1){return arr;}
+    let mid = Math.floor(arr.length/2);
+    let left = refinedMergeSort(arr.slice(0, mid));
+    let right = refinedMergeSort(arr.slice(mid));
+    return refinedMerge(left, right);
+    
 
+    //return refinedMergeSort(refinedMerge());
 }
 
+function refinedMerge(arr1, arr2){
+let results = [];
+let j = 0;
+let i = 0;
+while(i< arr1.length && j<arr2.length){
+    if(arr2[j]>arr1[i]){
+        results.push(arr1[i]);
+        i++;
+    } else {
+        results.push(arr2[j]);
+        j++;
+    }
+}
+while(i<arr1.length){
+    results.push(arr1[i]);
+    i++;
+}
 
+while(j<arr2.length){
+    results.push(arr2[j]);
+    j++;
+}
 
+return results;
+}
+
+console.log(refinedMergeSort([10,24,76,73]));
